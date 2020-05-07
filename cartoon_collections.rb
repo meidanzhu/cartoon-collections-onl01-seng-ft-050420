2fe39fab44443d11d2fbc16bc7aae405b0d4d055
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-07 01:03:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,14 @@ def roll_call_dwarves(dwarves)# code an argument here
     end
 end
 
-def summon_captain_plane(captain)
-  captain.map { |planeteer| planeteer.capitalize + "!" }
+def summon_captain_planet(captain)
+  captain = []
+  i = 0
+  while i < array.length
+    nu_array << array[i].capitalize + "!"
+    i += 1
+  end
+  captain
 end
 
 def long_planeteer_calls(calls)
@@ -13,8 +19,9 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
-  cheese.find do |item|
-  cheese_types.include?(item)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |type|
+  cheese_types.include?(type)
+
   end
 end
